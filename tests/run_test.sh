76d272fb81bb690c2e4e5de77710c63ff9819513
Will Rowe <will.rowe@stfc.ac.uk>
2018-03-09 12:15:22 +0000

    increasing mem requirments

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -6,7 +6,7 @@ curl -s https://get.nextflow.io | bash
 
 # run the test
 run_name="Test drax Run: "$(date +%s)
-cmd="./nextflow run ../main.nf -name \"${run_name}\" -profile docker --reads \"*R{1,2}.fq.gz\""
+cmd="./nextflow run ../main.nf -name \"${run_name}\" -profile docker --max_cpus 2 --max_memory '6.GB' --reads \"*R{1,2}.fq.gz\""
 echo "Starting nextflow... Command:"
 echo $cmd
 echo "-------------------------------------------------------"
