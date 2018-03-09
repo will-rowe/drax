7fd06ec09d0950f0a084dbeb0611105a0dce505c
Will Rowe <will.rowe@stfc.ac.uk>
2018-03-09 13:43:20 +0000

    trying conda instead of docker for travis

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git clone https://github.com/will-rowe/drax && cd drax/tests
 run_name="Test drax Run: "$(date +%s)
-cmd="./nextflow run ../main.nf -name \"${run_name}\" --max_cpus 2 --max_memory '6.GB' --reads \"*R{1,2}.fq.gz\""
+cmd="nextflow run ../main.nf -name \"${run_name}\" --max_cpus 2 --max_memory '6.GB' --reads \"*R{1,2}.fq.gz\""
 echo "Starting nextflow... Command:"
 echo $cmd
 echo "-------------------------------------------------------"
