a1220ebe4725b7f3564f8d13741ec1726179b080
Will Rowe <will.rowe@stfc.ac.uk>
2018-03-12 09:50:53 +0000

    updating test

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git clone https://github.com/will-rowe/drax && cd drax/tests
+git clone https://github.com/will-rowe/drax drax-repo && cd drax-repo/tests
 run_name="Test drax Run: "$(date +%s)
 cmd="nextflow run ../main.nf -name \"${run_name}\" --max_cpus 1 --max_memory '7.GB' --reads \"*R{1,2}.fq.gz\""
 echo "Starting nextflow... Command:"
