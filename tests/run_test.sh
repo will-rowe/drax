48d1a6cd5cf20a91bb937269c54fcd7bfb2d6004
Will Rowe <will.rowe@stfc.ac.uk>
2018-03-28 08:50:02 +0100

    fixing tests

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -3,13 +3,13 @@ git clone https://github.com/will-rowe/drax drax-repo && cd drax-repo/tests
 
 # set up dummy refData as real data too large for build tests
 mkdir DRAX-data && cd DRAX-data
-groot get
-mv arg-annot.90 grootDB
+groot get -d groot-core-db
+mv groot-core-db.90 grootDB
 cd ..
 
 # run tests
 run_name="Test drax Run: "$(date +%s)
-cmd="nextflow run will-rowe/drax -name \"${run_name}\" --max_cpus 1 --max_memory '7300.MB' --reads \"./*R{1,2}.fastq.gz\" --refData ./DRAX-data --decontaminate false  --noTaxa true"
+cmd="nextflow run ../main.nf -name \"${run_name}\" --max_cpus 1 --max_memory '7300.MB' --reads \"./*R{1,2}.fastq.gz\" --refData ./DRAX-data --decontaminate false  --noTaxa true"
 echo "Starting nextflow... Command:"
 echo $cmd
 echo "-------------------------------------------------------"
